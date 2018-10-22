7100de706ac6accc33e6aaaedce98f957a3536f0
BetweenTwoLights <danielle.rdb@outlook.com>
2018-10-22 16:45:14 -0500

    installing packages, setting up initial pages and routes

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
